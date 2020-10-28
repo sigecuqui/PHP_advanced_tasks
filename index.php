d587652caa363e3495181913340b6dc9c2b4dcda
sigecuqui <sigita.guogaite@gmail.com>
2020-10-28 16:03:46 +0200

    functions: names Upper Lower

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
 
-/**
- * Sukuria surodyto dydžio kvadratą.
- * @param $size integer Eilučių ir stulpelių kaičius.
- * @return array
- */
-function generate_matrix($size) {
-    $generated_array = [];
+$names = [
+    'beavis',
+    'dZIGIS',
+    'krakamule',
+    'kontrimas',
+    'BARTOLOMEJUS',
+    'stase'
+];
 
-    for ($i = 0; $i < $size; $i++) {
-
-        for ($j = 0; $j < $size; $j++) {
-            $generated_array[$i][$j] = rand(0, 1);
-        }
+//ucwords — Uppercase the first character of each word in a string
+//strtolower — Lowercase
+function changed_names($names) {
+    $changed = [];
+    foreach ($names as $name) {
+        $changed[] = ucwords(strtolower($name));
     }
-
-    return $generated_array;
+    return $changed;
 }
+$x = changed_names($names);
 
-$matrix = generate_matrix(rand(2, 5));
-
+var_dump($x);
 ?>
 <!doctype html>
 <html lang="en">
@@ -29,30 +30,8 @@ $matrix = generate_matrix(rand(2, 5));
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Functions</title>
-    <style>
-        div {
-            display: flex;
-        }
-        span {
-            width: 80px;
-            height: 80px;
-            margin: 5px;
-        }
-        .blue {
-            background: darkcyan;
-        }
-        .gold {
-            background: gold;
-        }
-    </style>
 </head>
 <body>
-<?php foreach ($matrix as $row): ?>
-    <div>
-        <?php foreach ($row as $col): ?>
-            <span class="<?php print $col ? 'blue' : 'gold'; ?>"></span>
-        <?php endforeach; ?>
-    </div>
-<?php endforeach; ?>
+
 </body>
 </html>
