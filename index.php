034a96b3782acc9f79051119c43c8e4162c95344
sigecuqui <sigita.guogaite@gmail.com>
2020-10-28 17:01:48 +0200

    functions: cards 5

--- index.php
+++ index.php
@@ -1,22 +1,36 @@
 <?php
-$suits = ['spades', 'diamonds', 'hearts', 'clubs'];
+$suits = ['cirvai', 'bugnai', 'vynai', 'kryziai'];
 $cards = ['A', 'K', 'Q', 'J', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-function cards_five($suits, $cards) {
-    $five_cards = [];
+//sudaryti 56 kortas
+function cards_block($suits, $cards) {
+    $cards_many = [];
     foreach ($suits as $suit) {
         foreach ($cards as $card) {
-            $five_cards[] = [
+            //priskirti suits prie suits, o cards prie cards
+            $cards_many[] = [
                 'suit' => $suit,
                 'card' => $card
             ];
         }
     }
+    return $cards_many;
+}
+
+$cards_deck = cards_block($suits, $cards);
+var_dump($cards_deck);
+
+function cards_on_table($array) {
+    $five_cards = [];
+    for ($i = 1; $i <= 5; ++$i) {
+        $random_index = rand(0, count($array) - 1);
+        $five_cards[] = $array[$random_index];
+        array_splice($array, $random_index, 1);
+    }
     return $five_cards;
 }
-$deck = cards_five($suits, $cards);
+$cards_on_table = cards_on_table($cards_deck);
 
-var_dump($deck);
 ?>
 <!doctype html>
 <html lang="en">
@@ -28,5 +42,8 @@ var_dump($deck);
     <title>Functions</title>
 </head>
 <body>
+<main>
+    <h1></h1>
+</main>
 </body>
 </html>
