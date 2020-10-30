e49adedbfadf25b466427a238234d8a7f7406504
sigecuqui <sigita.guogaite@gmail.com>
2020-10-30 16:05:01 +0200

    functions. actors

--- index.php
+++ index.php
@@ -1,15 +1,52 @@
 <?php
-$atsakymas = '';
-if (isset($_POST['submit'])) {
-    if ($_POST['submit'] === 'kelti') {
-        $sum = $_POST['number']*$_POST['number'];
-        $atsakymas = $sum;
+$movies = [
+    [
+        'image' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Leon-poster.jpg',
+        'name' => 'Leon: The Professional',
+        'kurejas' => 'Luc Besson',
+        'metai' => 1994,
+        'zanras' => ['Veiksmo', 'Drama'],
+        'aktoriai' => ['Natalie Portman', 'Jean Reno', 'Gary Oldman']
+    ],
+    [
+        'image' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR107,0,630,1200_AL_.jpg',
+        'name' => 'The Godfather',
+        'kurejas' => 'Francis Ford Coppola',
+        'metai' => 1972,
+        'zanras' => ['Kriminalinis', 'Drama'],
+        'aktoriai' => ['Al Pacino', 'Marlon Brando', 'James Caan']
+    ],
+    [
+        'image' => 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/Girl%2C_Interrupted_%281999%29.png/220px-Girl%2C_Interrupted_%281999%29.png',
+        'name' => 'Girl, Interrupted',
+        'kurejas' => 'James Mangold',
+        'metai' => 1999,
+        'zanras' => ['Drama'],
+        'aktoriai' => ['Angelina Jolie', 'Winona Ryder', 'Brittany Murphy']
+    ],
+];
+
+function filter($movies, $year) {
+    $filtered_list = [];
+    foreach ($movies as $value) {
+        if ($value['metai'] > $year) {
+            $filtered_list[] = $value;
+        }
     }
-    if($_POST['submit'] === 'saknis'){
-        $sum = sqrt($_POST['number']);
-        $atsakymas = $sum;
+    return $filtered_list;
+}
+
+$movie_filter = filter($movies, 1990);
+
+function add_name(&$movies, $movie, $actors) {
+    foreach ($movies as &$value) {
+        if ($value['name'] === $movie) {
+            $value['aktoriai'][] = $actors;
+        }
     }
 }
+
+add_name($movies, 'Leon: The Professional', 'Sige Gige');
 ?>
 <!doctype html>
 <html lang="en">
@@ -24,11 +61,19 @@ if (isset($_POST['submit'])) {
     </style>
 </head>
 <body>
-<h1><?php print $atsakymas; ?></h1>
-<form method="POST">
-    <input type="number" name='number'>
-    <input type="submit" name='submit' value = 'kelti'>
-    <input type="submit" name='submit' value = 'saknis'>
-</form>
+<main>
+    <?php foreach ($movies as $movie): ?>
+        <article class="movie_card">
+            <img src="<?php print $movie['image']; ?>" alt="image">
+            <h2><?php print $movie['name']; ?></h2>
+            <h3><?php print $movie['kurejas']; ?></h3>
+            <h3><?php print $movie['metai']; ?></h3>
+            <h3><?php print implode(', ', $movie['zanras']); ?></h3>
+            <ul>
+                <li><?php print implode(', ', $movie['aktoriai']); ?></li>
+            </ul>
+        </article>
+    <?php endforeach; ?>
+</main>
 </body>
 </html>
