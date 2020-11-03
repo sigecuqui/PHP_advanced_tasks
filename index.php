aeb50498605be27d99b0dbbd2980bcf1a187de58
sigecuqui <sigita.guogaite@gmail.com>
2020-11-03 11:57:10 +0200

    forma, didejantis bananas

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
-$description = '';
-$name = '';
-$surname = '';
-$age = '';
-$level = '';
-$year = '';
+$value = 0;
+$size = 50;
 
-if (isset($_POST['submit'])) {
-    if ($_POST['submit'] === 'PATEIKTI') {
-        $name = $_POST['name'];
-        $surname = $_POST['surname'];
-        $age = $_POST['age'];
-        $year = 2020 - (int)$age;
-        $level = $_POST['level'];
-        $description = "$name, gimęs(-usi) $year, yra $level PHP programuotojas.";
-        $_POST['form'] = '';
-    }
+if (isset($_POST['button'])) {
+    $value = (int)$_POST['button'] + 1;
+    $size = (int)$_POST['button'] * 10;
 }
 ?>
 <!doctype html>
@@ -26,35 +15,20 @@ if (isset($_POST['submit'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Forma</title>
+    <title>Banana</title>
     <style>
+        img {
+            width: <?php print $size; ?>px;
+        }
     </style>
 </head>
 <body>
 <main>
-    <?php if (isset($_POST['submit'])): ?>
-        <div class="block">
-            <p>Vardas: <?php print $name; ?></p>
-            <p>Pavardė: <?php print $surname; ?></p>
-            <p>Amžius: <?php print $age; ?></p>
-            <p>Lygis: <?php print $level; ?></p>
-            <p class="description"><?php print $description; ?></p>
-        </div>
-    <?php else: ?>
-        <h1>Užpildyk formą:</h1>
-        <form method="POST" name="form">
-            <input type="text" name="name" placeholder="Vardas">
-            <input type="text" name="surname" placeholder="Pavardė">
-            <input type="number" name="age" placeholder="Amžius">
-            <label id="options">Kaip vertini savo PHP žinias?</label>
-            <select name="level" id="options">
-                <option value="prastas">Prastai</option>
-                <option value="neblogas">Neblogai</option>
-                <option value="puikus">Puikiai</option>
-            </select>
-            <input type="submit" name="submit" value="PATEIKTI">
-        </form>
-    <?php endif; ?>
+    <form method="post">
+        <button name="button" value="<?php print $value; ?>"><?php print $value; ?></button>
+        <img src="https://i1.wp.com/freepngimages.com/wp-content/uploads/2015/11/banana-transparent-background.png?fit=600%2C300"
+             alt="banana">
+    </form>
 </main>
 </body>
 </html>
