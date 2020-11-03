ea1a8e46b46291dc63bb6979ff791bd15341c765
sigecuqui <sigita.guogaite@gmail.com>
2020-11-03 16:33:18 +0200

    functions

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php
-$sentence = 'vagi vagi vagi na';
+$answer = '';
 
-function change(&$sentence) {
-    $str_implode = str_split($sentence);
-
-    $caps = true;
-
-    foreach ($str_implode as $key => $letter) {
-        if ($caps) {
-            $out = strtoupper($letter);
-            if ($out !== " ") //not a space character
-                $caps = false;
+function square(&$answer) {
+    if (isset($_POST['number'])) {
+        if (is_numeric($_POST['number'])) {
+            $answer = $_POST['number'] * $_POST['number'];
         } else {
-            $out = strtolower($letter);
-            $caps = true;
+            $answer = 'Please enter a number';
         }
-        $str_implode[$key] = $out;
     }
-
-    $sentence = implode('', $str_implode);
 }
-change($sentence);
-var_dump($sentence);
+
+square($answer);
 ?>
 <!doctype html>
 <html lang="en">
@@ -31,10 +21,15 @@ var_dump($sentence);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Functions</title>
+    <title>Forma</title>
 </head>
 <body>
 <main>
+    <form method="post">
+            <input name="number" type="text">
+            <button type="submit" name="button">Submit</button>
+            <p><?php print $answer; ?></p>
+    </form>
 </main>
 </body>
 </html>
