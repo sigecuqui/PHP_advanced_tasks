1e2598b0a6ce97b15f686d5cab786872692a3d44
sigecuqui <sigita.guogaite@gmail.com>
2020-11-05 16:16:48 +0200

    Forma su php, html, submitas

--- index.php
+++ index.php
@@ -1,50 +1,128 @@
 <?php
+function get_subject($subject)
+{
+    $selected_subject = '';
+    switch ($subject) {
+        case 1:
+        {
+            $selected_subject = 'Skundas';
+            break;
+        }
+        case 2:
+        {
+            $selected_subject = 'Pasiūlymas';
+            break;
+        }
+    }
 
+    return $selected_subject;
+}
 
+function get_department($department)
+{
+    $selected_department = '';
+    switch ($department) {
+        case 'sales':
+        {
+            $selected_department = 'Pardavimų Skyrius';
+            break;
+        }
+        case 'management':
+        {
+            $selected_department = 'Administracija';
+            break;
+        }
+        case 'support':
+        {
+            $selected_department = 'Klientų aptarnavimo skyrius';
+            break;
+        }
+    }
+    return $selected_department;
+}
+
+$user_data = [];
+$show_form = true;
+
+if (!empty($_POST)) {
+    $user_data['name'] = 'Vardas: ' . ($_POST['name']);
+    $user_data['email'] = 'El. pašto adresas: ' . $_POST['email'];
+    $user_data['phone'] = 'Telefono numeris: ' . $_POST['phone'];
+    $user_data['help'] = 'Skirta: ' . get_department($_POST['help']);
+    $user_data['subject'] = 'Tema: ' . get_subject($_POST['subject']);
+    $user_data['message'] = 'Žinutė: ' . $_POST['message'];
+    $show_form = false;
+}
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Forms</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Service</title>
     <style>
-       div {
-           display: flex;
-           flex-direction: column;
-           margin: 20px;
-       }
 
-       form {
-           display: flex;
-           flex-direction: column;
-           width: 300px;
-       }
     </style>
 </head>
 <body>
-<main>
-    <form method="post">
-        <input type="text" name="vardas" placeholder="Jūsų vardas" required>
-        <input type="email" name="email" placeholder="El. paštas" required>
-        <input type="tel" id="phone" name="phone" placeholder="Telefono numeris">
-        <div>
-            NORIU SUSISIEKTI SU:
-        <input type="radio" name="choose" required><p>Pardavimų skyriumi</p>
-        <input type="radio" name="choose"><p>Administracija</p>
-        <input type="radio" name="choose"><p>Klientų aptarnavimo skyriumi</p>
+<?php if ($show_form): ?>
+    <main id="block">
+        <header class="bg">
+            <div class="bg-overlay">
+                <h1>parašykite mums žinutę</h1>
+            </div>
+        </header>
+        <section class="form-block">
+            <form method="post">
+                <div class="name-input">
+                    <input type="text" placeholder="Jūsų vardas" name="name" required>
+                </div>
+                <div class="email-phone">
+                    <input type="email" placeholder="El. Paštas" email.com name="email" required>
+                    <input type="tel" placeholder="Telefono numeris" name="phone" pattern="[0-9]{3}[0-9]{2}[0-9]{4}"
+                    >
+                </div>
+                <div class="help-radio">
+                    <h4>Noriu susisiekti su:</h4>
+                    <div class="radio-center">
+                        <input type="radio" name="help" value="sales" id="sales" required>
+                        <label for="sales">Pardavimų skyriumi</label>
+                    </div>
+                    <div class="radio-center">
+                        <input type="radio" name="help" value="management" id="management">
+                        <label for="management">Administracija</label>
+                    </div>
+                    <div class="radio-center">
+                        <input type="radio" name="help" value="support" id="support">
+                        <label for="support">Klientų aptarnavimo skyriumi</label>
+                    </div>
+                </div>
+                <select name="subject">
+                    <option value="select">pasirinkite temą</option>
+                    <option value="1">Skundas</option>
+                    <option value="2">Pasiūlymas</option>
+                </select>
+                <div>
+                    <textarea name="message" placeholder="Jūsų žinutė.." cols="20" rows="3"></textarea>
+                </div>
+                <div class="btn">
+                    <button type="submit">Siųsti</button>
+                </div>
+            </form>
+        </section>
+    </main>
+<?php else: ?>
+    <div class="message-block">
+        <h3>Dėkojame už jūsų žinutę, <?php print $_POST['name']; ?></h3>
+        <div class="message-info">
+            Jūsų pateikta informacija:
+            <?php foreach ($user_data as $user_info): ?>
+                <p><?php print $user_info; ?></p>
+            <?php endforeach; ?>
         </div>
-        <select id="options" name="options">
-            <option value="choose" label="*PASIRINKITE TEMĄ*" disabled></option>
-            <option value="skundas">SKUNDAS</option>
-            <option value="pasiulymas">PASIŪLYMAS</option>
-            <option value="info">BENDRA INFORMACIJA</option>
-        </select>
-        <textarea name="message" id="message" rows="5"></textarea>
-        <input type="submit" name="name" value="Siųsti">
-    </form>
-</main>
+    </div>
+<?php endif; ?>
 </body>
+
 </html>
